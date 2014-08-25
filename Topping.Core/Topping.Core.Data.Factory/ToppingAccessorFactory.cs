063dc1251d1ef568c0f0e46f16a2067e5bdeeb15
dvanbelle <dvanbelle@ict7.com>
2014-08-25 10:06:28 +0200

    new version
    version mono

---
+++ Topping.Core/Topping.Core.Data.Factory/ToppingAccessorFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Topping.Core.Data.Factory
+{
+    public abstract class ToppingAccessorFactory
+    {
+        public abstract IToppingAccessor GetContext();
+    }
+
+    public  class ConcreteToppingAccessorFactory : ToppingAccessorFactory
+    {
+
+        public override IToppingAccessor GetContext()
+        {
+            if (true)
+            {
+				return (IToppingAccessor)new Topping.Core.Data.SQL.ToppingAccessor();
+            }
+            else 
+            {
+				return (IToppingAccessor)new Topping.Core.Data.Db4o.ToppingAccessor();
+            }
+        }
+    }
+}
